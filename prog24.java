a67f78bc347575feab676c1ac02eea45ca4cb72f
pankaj10664 <pankaj10664@gmail.com>
2018-06-09 20:02:52 +0530

    print  your name

---
+++ prog24.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Pankaj
+{
+public static void main(String args[])
+
+{
+	String n;
+Scanner obj = new Scanner(System.in);
+System.out.println("Enter your name");
+n = obj.nextLine();
+System.out.print("your name is"+n);
+}
+}
